27116006f1445c619d243b7f11fcecab05ecf822
aaronev <aarone.villanueva@gmail.com>
2017-08-14 13:17:09 -0700

    modifies routes folder and files

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-router.use('/', require('./routers/home'))
-router.use('/albums', require('./routers/albums'))
-router.use('/reviews', require('./routers/reviews'))
-router.use('/users', require('./routers/users'))
-
-module.exports = router
---
+++ src/routes/sign-up.js
@@ -0,0 +1,18 @@
+router.route('/sign-up')
+  .get((req, res) => { 
+    res.render('sign-up') 
+  })
+  .post((req, res, next) => {
+    const {name, email, password} = req.body
+    users.findByEmail(email)
+    .then(foundEmail => {
+      if (foundEmail) {
+        req.flash('errorSignUp', 'Email already exist!')
+        res.redirect('/sign-up')
+      } else {
+        users.create(name, email, password, '/img/no-dj.png')
+        .then(addedUsers => { res.redirect('/authenticate/sign-in') })
+        .catch(next)
+      }
+    }).catch(next)
+  })
