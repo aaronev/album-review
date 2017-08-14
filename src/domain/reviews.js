e44a732c848cc5b318bf6d5b218779111d1080c5
aaronev <aarone.villanueva@gmail.com>
2017-08-14 11:30:23 -0700

    adds reviews abstract functions

--- src/domain/reviews.js
+++ src/domain/reviews.js
@@ -1,4 +1,24 @@
-/*
-const DBTable = require(../database/database)
-const DBReviews = new DBTable('reviews', [])
-*/
+const DBTable = require('../database/database')
+const DBReviews = new DBTable('reviews', ['user_id', 'album_id', 'review'])
+
+const reviews = {}
+
+reviews.all = () => 
+  DBReviews.all()
+
+reviews.latest3 = () => 
+  DBReviews.limit(3)
+
+reviews.create = (userID, albumID, review) =>
+  DBReviews.insert([userID, albumID, review])
+
+reviews.findByUserID = (userID) => 
+  DBReviews.find('user_id', userID)
+
+reviews.findByAlbumID = (albumID) => 
+  DBReviews.find('album_id', albumID)
+
+reviews.delete = (reviewID) => 
+  DBReviews.delete('id', reviewID)
+
+module.exports = reviews
