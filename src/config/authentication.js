a688aaabd53961936ac75a2485015eaa13440829
aaronev <aarone.villanueva@gmail.com>
2017-08-14 13:19:52 -0700

    adds configured  passport authentication

--- src/config/authentication.js
+++ src/config/authentication.js
@@ -1,8 +1,41 @@
-/*
-
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const users = require('../domain/users')
 
+passport.serializeUser((user, done) => { 
+  done(null, user.id) 
+})
 
-create passport configurations
-*/
+passport.deserializeUser((id, done) => {
+  users.findByID(id)
+  .then(users => done(null, users)) 
+})
+
+passport.use('local', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password', 
+    passReqToCallback: true
+  }, 
+  (req, email, plainTextPassword, done) => {
+    users.findByEmail(email)
+    .then(user => {
+      if (!user) {
+        return done(null, false, req.flash(
+          'errorLogin', 
+          'Email not found, please sign up!'
+          ))
+      } else {
+        return users.toVerifyPassword(
+          plainTextPassword, user.password
+        )
+        ? done(null, user)
+        : done(null, false, req.flash(
+            'errorLogin', 
+            'Incorrect password!'
+        ))
+      }
+    })
+  }
+))
+
+module.exports = passport
