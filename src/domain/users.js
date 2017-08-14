e69de71a233c4388e3ca763fcc18a06236001b35
aaronev <aarone.villanueva@gmail.com>
2017-08-14 11:33:10 -0700

    adds user abstract functions

--- src/domain/users.js
+++ src/domain/users.js
@@ -1,4 +1,27 @@
-/*
-const DBTable = require(../database/database)
-const DBUsers = new DBTable('users', [])
-*/
+const bcrypt = require('bcrypt')
+const DBTable = require('../database/database')
+const DBUsers = new DBTable('users', ['name', 'email', 'password', 'image'])
+
+const users = {}
+
+function encrypt(plainText) {
+  const salt = bcrypt.genSaltSync(10)
+  return bcrypt.hashSync(plainText, salt)
+}
+
+users.toVerifyPassword = (plainText, hashedText) =>
+  bcrypt.compareSync(plainText, hashedText)
+
+users.all = () => 
+  DBUsers.all()
+
+users.create = (name, email, password, image) =>
+  DBUsers.insert([name, email, encrypt(password), image])
+
+users.findByID = (ID) => 
+  DBUsers.find('id', ID).then(user => user[0])
+
+users.findByEmail = (email) => 
+  DBUsers.find('email', email).then(user => user[0])
+
+module.exports = users
